6bd1a69a76f106577877c4dacdec3381b37d14a5
AutumnDate <xzh19725@163.com>
2026-05-09 14:41:17 +0800

    相应地对 cpp 文件做出了修改

--- expTest/mainwindow.cpp
+++ expTest/mainwindow.cpp
@@ -5,23 +5,23 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <linux/input.h> // [新增]
+#include <linux/input.h> 
 #include <sys/mman.h>
 
-// NFC 指令保持不变...
 unsigned char wakeup_cmd[] = {0x55,0x55,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xff,0x03,0xfd,0xd4,0x14,0x01,0x17,0x00};
 unsigned char getUID_cmd[] = {0x00, 0x00, 0xFF, 0x04, 0xFC, 0xD4, 0x4A, 0x01, 0x00, 0xE1, 0x00};
 
 static unsigned char tube_code[] = {0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90,0x7f,0xff};
 static unsigned char addr_code[] = {0x11,0x22,0x44,0x88};
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+// <--- 修改：构造函数作用域 --->
+AttendanceSystem::AttendanceSystem(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::AttendanceSystem) // <--- 修改：实例化对应的 UI 类
 {
     ui->setupUi(this);
 
-    // --- 1. 初始化 NFC 串口 ---
+    // NFC 初始化
     fd_nfc = com_init("/dev/ttyS2", B115200);
     if(fd_nfc > 0) {
         ::write(fd_nfc, wakeup_cmd, sizeof(wakeup_cmd));
@@ -30,26 +30,21 @@ MainWindow::MainWindow(QWidget *parent) :
         nfcTimer->start(50);
     }
 
-    // --- 2. 初始化 LED 驱动 ---
+    // LED 初始化
     fd_led = ::open("/dev/ledtest", O_RDWR);
     ledOffTimer = new QTimer(this);
     ledOffTimer->setSingleShot(true);
     connect(ledOffTimer, SIGNAL(timeout()), this, SLOT(turnOffAllLeds()));
 
-    // --- 3. 初始化 Beep 驱动 [新增] ---
+    // Beep 初始化
     fd_beep = ::open("/dev/input/event1", O_RDWR);
-    if(fd_beep < 0) {
-        perror("beep device open failed");
-    }
 
+    // 数码管初始化
     displayNum = 0;
     currentDigit = 0;
     fd_mem = ::open("/dev/mem", O_RDWR);
     if(fd_mem > 0) {
-        // 映射物理地址 0x8000000
         cpld_ptr = (unsigned char*)mmap(NULL, (size_t)0x10, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, fd_mem, (off_t)(0x8000000));
-        
-        // 启动高频扫描定时器 (5ms 刷新一次)
         segTimer = new QTimer(this);
         connect(segTimer, SIGNAL(timeout()), this, SLOT(refreshSegmentDisplay()));
         segTimer->start(5); 
@@ -58,59 +53,43 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->lbl_Status->setText("系统就绪，请刷卡...");
 }
 
-MainWindow::~MainWindow()
+// <--- 修改：析构函数作用域 --->
+AttendanceSystem::~AttendanceSystem()
 {
     if(fd_nfc > 0) ::close(fd_nfc);
     if(fd_led > 0) ::close(fd_led);
-    if(fd_beep > 0) ::close(fd_beep); // [新增]
-
+    if(fd_beep > 0) ::close(fd_beep); 
     if(cpld_ptr != MAP_FAILED) munmap(cpld_ptr, 0x10);
     if(fd_mem > 0) ::close(fd_mem);
-
     delete ui;
 }
 
-// ================= [新增/修改] 实现 Beep 控制逻辑 =================
+// <--- 以下所有函数前缀全部从 MainWindow:: 改为 AttendanceSystem:: --->
 
-void MainWindow::triggerBeep(int ms) {
+void AttendanceSystem::triggerBeep(int ms) {
     if(fd_beep < 0) return;
-
     struct input_event event;
-    event.type = EV_SND;
-    event.code = SND_BELL;
-    event.value = 1; // 鸣叫
+    event.type = EV_SND; event.code = SND_BELL; event.value = 1;
     ::write(fd_beep, &event, sizeof(struct input_event));
-
-    // 使用 Qt 的单次定时器，在 ms 毫秒后调用关闭函数，不阻塞界面
     QTimer::singleShot(ms, this, SLOT(turnOffBeep()));
 }
 
-void MainWindow::turnOffBeep() {
+void AttendanceSystem::turnOffBeep() {
     if(fd_beep < 0) return;
-
     struct input_event event;
-    event.type = EV_SND;
-    event.code = SND_BELL;
-    event.value = 0; // 停止
+    event.type = EV_SND; event.code = SND_BELL; event.value = 0;
     ::write(fd_beep, &event, sizeof(struct input_event));
 }
 
-// ================= LED 逻辑保持不变 =================
-void MainWindow::triggerLed(bool on) {
+void AttendanceSystem::triggerLed(bool on) {
     if(fd_led < 0) return;
     int status = on ? 1 : 0;
-    for(int i = 0; i < 4; i++) {
-        ::ioctl(fd_led, status, i);
-    }
+    for(int i = 0; i < 4; i++) { ::ioctl(fd_led, status, i); }
 }
 
-void MainWindow::turnOffAllLeds() {
-    triggerLed(false);
-}
+void AttendanceSystem::turnOffAllLeds() { triggerLed(false); }
 
-// ================= NFC 任务逻辑 [修改] =================
-
-void MainWindow::readNfcTask() {
+void AttendanceSystem::readNfcTask() {
     unsigned char c;
     if(::read(fd_nfc, &c, 1) == 1) {
         for(int i=0; i<24; i++) { uartdata[i] = uartdata[i+1]; }
@@ -123,26 +102,19 @@ void MainWindow::readNfcTask() {
                 && uartdata[5]==0x00 && uartdata[6]==0x00 && uartdata[7] == 0x00 && uartdata[8] == 0xFF && uartdata[12] == 0x4b && uartdata[18] == 0x04 && uartdata[24] == 0x00) 
         {
             unsigned long uid = uartdata[19]<<24 | uartdata[20]<<16 | uartdata[21]<<8 | uartdata[22];
-            
             ui->lbl_CardID->setText(QString("UID: 0x%1").arg(uid, 8, 16, QChar('0')));
             ui->lbl_Status->setText("签到成功！");
-
             triggerLed(true);
             ledOffTimer->start(2000); 
-
-            // --- 触发蜂鸣器 [修改] ---
-            triggerBeep(200); // 响 200 毫秒
-
+            triggerBeep(200);
             updateSegmentLed(uid);
             saveToDatabase(uid);
-
             ::write(fd_nfc, getUID_cmd, sizeof(getUID_cmd));
         }
     }
 }
 
-// 其余函数 (com_init等) 保持不变...
-int MainWindow::com_init(const char* device, speed_t speed) {
+int AttendanceSystem::com_init(const char* device, speed_t speed) {
     struct termios options;
     int fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
     if(fd < 0) return -1;
@@ -159,30 +131,20 @@ int MainWindow::com_init(const char* device, speed_t speed) {
     return fd;
 }
 
-void MainWindow::updateSegmentLed(long id) {
-    // 将卡号的后四位取出来显示
-    displayNum = id % 10000;
-}
+void AttendanceSystem::updateSegmentLed(long id) { displayNum = id % 10000; }
 
-// <--- 新增：实现 refreshSegmentDisplay 扫描逻辑 (对应 demo.c 的 switch 部分) --->
-void MainWindow::refreshSegmentDisplay() {
+void AttendanceSystem::refreshSegmentDisplay() {
     if(cpld_ptr == MAP_FAILED) return;
-
     int val = 0;
     switch(currentDigit) {
-        case 0: val = displayNum % 10; break;           // 个位
-        case 1: val = (displayNum % 100) / 10; break;   // 十位
-        case 2: val = (displayNum % 1000) / 100; break; // 百位
-        case 3: val = displayNum / 1000; break;         // 千位
+        case 0: val = displayNum % 10; break;
+        case 1: val = (displayNum % 100) / 10; break;
+        case 2: val = (displayNum % 1000) / 100; break;
+        case 3: val = displayNum / 1000; break;
     }
-
-    // 控制 CPLD 寄存器 (参考 demo.c 逻辑)
-    *(cpld_ptr + (0xe6 << 1)) = addr_code[currentDigit]; // 设置位置
-    *(cpld_ptr + (0xe4 << 1)) = tube_code[val];         // 设置段码
-
-    // 准备下一位扫描
+    *(cpld_ptr + (0xe6 << 1)) = addr_code[currentDigit];
+    *(cpld_ptr + (0xe4 << 1)) = tube_code[val];
     currentDigit = (currentDigit + 1) % 4;
 }
-// <--- 新增结束 --->
 
-void MainWindow::saveToDatabase(long id) {}
+void AttendanceSystem::saveToDatabase(long id) {}
--- expTest/mainwindow.h
+++ expTest/mainwindow.h
@@ -1,58 +1,50 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QWidget>  // <--- 修改：从 QMainWindow 改为 QWidget
 #include <QTimer>
 #include <termios.h> 
-#include <linux/input.h> // [新增] 用于蜂鸣器 input_event 结构体
+#include <linux/input.h> 
 #include <sys/mman.h>
 
 namespace Ui {
-class MainWindow;
+class AttendanceSystem; // <--- 修改：名字必须与 .ui 里的 class 一致
 }
 
-class MainWindow : public QMainWindow
+class AttendanceSystem : public QWidget // <--- 修改：类名和基类
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    explicit AttendanceSystem(QWidget *parent = 0); // <--- 修改：构造函数名
+    ~AttendanceSystem(); // <--- 修改：析构函数名
 
 private slots:
     void readNfcTask();
     void triggerLed(bool on);       
     void turnOffAllLeds();          
-    
-    void triggerBeep(int ms);      // [修改] 稍后在cpp中实现
-    void turnOffBeep();            // [新增] 专门用于关闭蜂鸣器的槽函数
-
-    void refreshSegmentDisplay();  // 新增：高频扫描槽函数
-    
+    void triggerBeep(int ms);      
+    void turnOffBeep();            
+    void refreshSegmentDisplay();  
     void updateSegmentLed(long id); 
     void saveToDatabase(long id);   
 
 private:
-    Ui::MainWindow *ui;
+    Ui::AttendanceSystem *ui; // <--- 修改：Ui 类型名
 
-    // --- NFC 变量 ---
+    // 硬件变量保持不变
     int fd_nfc;
     QTimer *nfcTimer;
     unsigned char uartdata[25];
     int com_init(const char* device, speed_t speed);
-
-    // --- LED 变量 ---
     int fd_led;                     
     QTimer *ledOffTimer;            
-
-    // --- Beep 变量 ---
-    int fd_beep;                    // [新增] 蜂鸣器文件描述符
-
-    int fd_mem;                     // /dev/mem 文件描述符
-    unsigned char *cpld_ptr;        // 映射后的内存指针
-    int displayNum;                 // 当前数码管显示的数值
-    int currentDigit;               // 当前正在扫描的位数 (0-3)
-    QTimer *segTimer;               // 数码管扫描定时器
+    int fd_beep;                    
+    int fd_mem;                     
+    unsigned char *cpld_ptr;        
+    int displayNum;                 
+    int currentDigit;               
+    QTimer *segTimer;               
 };
 
 #endif
